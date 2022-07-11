5e8a949bd124119a9b308a31d6141856b68c3cf1
sbwaebeyt <sbwaebeytt@gmail.com>
2022-07-11 05:26:05 +0300

    Implement own path system (really?)

--- libman-api/src/main/java/pw/iwmc/libman/api/downloader/Downloader.java
+++ libman-api/src/main/java/pw/iwmc/libman/api/downloader/Downloader.java
@@ -2,6 +2,7 @@ package pw.iwmc.libman.api.downloader;
 
 import org.jetbrains.annotations.NotNull;
 import pw.iwmc.libman.api.objects.Dependency;
+import pw.iwmc.libman.api.objects.Repository;
 
 /**
  * Main of a ${@link Downloader}
@@ -13,4 +14,28 @@ public interface Downloader {
      * @param dependency the dependency.
      */
     void downloadDependency(@NotNull Dependency dependency);
+
+    /**
+     * Downloads dependency.
+     *
+     * @param dependency the dependency.
+     * @param repository the repository.
+     *
+     * @return true if dependency successfuly downloaded.
+     *
+     * @throws Exception if an error occurs during execution.
+     */
+    boolean downloadDependency(Dependency dependency, @NotNull Repository repository) throws Exception;
+
+    /**
+     * Downloads snapshot dependency.
+     *
+     * @param dependency the dependency.
+     * @param repository the repository.
+     *
+     * @return true if dependency successfuly downloaded.
+     *
+     * @throws Exception if an error occurs during execution.
+     */
+    boolean downloadSnapshotDependency(Dependency dependency, @NotNull Repository repository) throws Exception;
 }
